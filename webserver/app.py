de479fb856fe7a22d53e7bcbd028198ac728ab8f
justice <81177526+tPeltier@users.noreply.github.com>
2025-03-27 13:56:47 -0400

    update server/app
    working on flask rewrite

--- webserver/app.py
+++ webserver/app.py
@@ -1,20 +1,66 @@
 from flask import Flask, jsonify
 import serial
+import sys # for parsing args
+from unittest.mock import MagicMock # for local development
 
-# port = "/dev/ttyACM0"
-# ser = serial.Serial(port) # TODO: need to figure out how to kill this??
+"""
+utilities
+"""
+
+def is_dev(args):
+    if len(args) > 1 and args[1] == 'local':
+        return True
+    else:
+        return False
+
+################################################################################
+
+"""
+serial setup
+"""
+
+serial_port = "/dev/ttyACM0"
+def create_serial_connection(serial_port):
+    # TODO: figure out how to kill
+    return serial.Serial(serial_port)
+
+# set up serial connection for dev or prod based on cli args
+if is_dev(sys.argv):
+    ser = MagicMock()
+else:
+    ser = create_serial_connection(serial_port)
+
+################################################################################
+
+"""
+application
+"""
+
+################################################################################
+
+"""
+server
+"""
 
 app = Flask(__name__)
 
 @app.route('/press')
 def press_button():
     print('Pressing button...') # WARN: debug
-    # ser.flush() # NOTE: necessary?
-    # ser.write(b'press')
+    ser.flush() # NOTE: necessary?
+    ser.write(b'press')
     print('Button Pressed') # WARN: debug
     return jsonify({'message': 'button pressed'})
 
+################################################################################
+
 if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = 80
-    app.run(host, port, debug=True)
+    if is_dev(sys.argv):
+        # for localhost
+        app.run(debug=True)
+    else:
+        # for pi
+        # TODO: work out host & port
+        host = '0.0.0.0'
+        port = 80
+        app.run(host, port, debug=True)
