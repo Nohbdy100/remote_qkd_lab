d067b37942d850503886e6e1364596f3dd6e24f0
Devin Thurman <97561558+Nohbdy100@users.noreply.github.com>
2025-09-28 18:59:59 -0400

    Delete core/urls.py

--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include   # 👈 include added here
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('appointments/', include('appointments.urls')),  # 👈 this links to your app
-]
